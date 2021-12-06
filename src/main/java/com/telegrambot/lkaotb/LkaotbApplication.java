61da331cc26cd5483a7ed353515a6fce9ea10075
Ksenia <ksenia.obertii@gmail.com>
2021-12-06 23:12:05 +0200

    First commit

---
+++ src/main/java/com/telegrambot/lkaotb/LkaotbApplication.java
@@ -0,0 +1,13 @@
+package com.telegrambot.lkaotb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkaotbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkaotbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telegrambot/lkaotb/LkaotbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegrambot.lkaotb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LkaotbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
